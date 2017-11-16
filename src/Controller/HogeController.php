56ff989ca575fa63683fb7d0954a18efb0a9a63d
nasunin1 <oic.a.tokuyama@gmail.com>
2017-11-16 17:47:03 +0900

    aaa

---
+++ src/Controller/HogeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class HogeController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function index()
+    {
+
+    }
+}
